0b3dd3f89c0f6b710e87254d88383af91da0693f
guliping <1058719078@qq.com>
2017-05-03 19:10:46 +0800

    新增std::bind 新增std::move 测试用例

--- CNewFeature/CNewFeature.cpp
+++ CNewFeature/CNewFeature.cpp
@@ -1,10 +1,24 @@
 // CNewFeature.cpp : 定义控制台应用程序的入口点。
 //
 
+
+/************************************************************************/
+/* 
+	1. 可变长模板参数测试
+	2. std::function,std::bind 测试 
+	3. std::move 测试
+
+*/
+/************************************************************************/
+
 #include "stdafx.h"
 #include <string>
 #include <stdio.h>
 #include <functional>
+#include <vector>
+#include <iostream>
+#include <random>
+#include <memory>
 
 class C11{
 
@@ -74,7 +88,7 @@ public:
 		return getJNISignature(x) + getJNISignature(param...);
 	}
 
-
+	//std::function
 	std::function<void()> mFunc;
 	void setFunc(std::function<void()> lam){
 		mFunc = lam;
@@ -87,20 +101,82 @@ public:
 
 };
 
+void f(int n1, int n2, int n3, const int& n4, int n5)
+{
+	std::cout << n1 << ' ' << n2 << ' ' << n3 << ' ' << n4 << ' ' << n5 << '\n';
+}
+
+int g(int n1)
+{
+	return n1 + 1;
+}
+
+struct Foo {
+	void print_sum(int n1, int n2)
+	{
+		std::cout << n1 << "+" << n2 << "=" << n1 + n2 << '\n';
+	}
+	int data = 10;
+};
+
 int _tmain(int argc, _TCHAR* argv[])
 {
+	//std::bind测试
+	int n = 7;
+	// (_1 and _2 are from std::placeholders, and represent future
+	// arguments that will be passed to f1)
+	auto f1 = std::bind(f, std::placeholders::_2, std::placeholders::_1, 42, std::cref(n), n);
+	n = 10;
+	f1(1, 2, 1001);
 
+	auto f2 = std::bind(f, std::placeholders::_3, std::bind(g, std::placeholders::_3), std::placeholders::_3, 4, 5);
+	f2(10, 11, 12);
+
+	// common use case: binding a RNG with a distribution
+	std::default_random_engine e;//随机数
+	std::uniform_int_distribution<> d(0, 10);
+	std::function<int()> rnd = std::bind(d, e); // a copy of e is stored in rnd
+	for (int n = 0; n < 10; ++n)
+		std::cout << rnd() << ' ';
+	std::cout << '\n';
+
+	// bind to a pointer to member function
+	Foo foo;
+	auto f3 = std::bind(&Foo::print_sum, &foo, 95, std::placeholders::_1);
+	f3(5);
+
+	printf("**********************************************\n");
+
+	//可变长模板参数
 	C11::test1("[%s] %d + %d = %d\n","add",1,2,3);
-
 	const char* s1 = "123456";
 
 	auto lam = [s1]()->void{
 		printf("%s\n", s1);
 	};
-
 	C11 obj;
 	obj.setFunc(lam);
 	obj.test2();
+	
+	printf("**********************************************\n");
+
+	//std::move 测试
+	std::string str = "Hello";
+	std::vector<std::string> v;
+
+	// uses the push_back(const T&) overload, which means 
+	// we'll incur the cost of copying str
+	v.push_back(str);
+	std::cout << "以前的深拷贝, str is \"" << str << "\"\n";
+
+	// uses the rvalue reference push_back(T&&) overload, 
+	// which means no strings will be copied; instead, the contents
+	// of str will be moved into the vector.  This is less
+	// expensive, but also means str might now be empty.
+	v.push_back(std::move(str));
+	std::cout << "现在c++11的move, str is \"" << str << "\"\n";
+
+	std::cout << "我们的字符串队列 \"" << v[0] << "\", \"" << v[1] << "\"\n";
 
 	getchar();
 
